dc54eca6004b7bdd93f87cffd58d6ef0a530a5fc
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-06-08 17:37:26 +0530

    Added Comments to the 'Scinetific' header file

--- Calculator/scientific.h
+++ Calculator/scientific.h
@@ -1,3 +1,6 @@
+/* Header Guard
+ * prevent the file from being included multiple times during compilation, which would cause errors.
+ */
 #ifndef SCIENTIFIC_H
 #define SCIENTIFIC_H
 
@@ -6,14 +9,42 @@
 #include <cmath>
 #include "calculator.h"
 
+/* This defines the 'Scientific' class, inheriting publicly from the 'Calculator' class.
+ * This means 'Scientific' will have access to all 'public' and 'protected' members of 'Calculator'.
+ * It extends functionality with additional scientific operations.
+ */
 class Scientific: public Calculator
 {
 public:
 	Scientific();
+	/* this 'Scientific' constructor, It may initialize additonal members or rely on the base 'Calculator' constructor for default values.
+	 */
 	virtual ~Scientific() = default;
+	/* Virtual destructor:
+	 * ensures that if a 'Scientific' object is deleted via a pointer to a 'Calculator', the proper destructor (from 'Scientific') is called.
+	 * = default: tells the compiler to generate the destructor automaically.
+	 */
+
+
+
+	/* Overriding Base Class Virtual Functions
+	 * welcome(): 
+	 * 	- Overrides the welcome() function from 'Calculator'.
+	 * 	- Will likely print a customized welcome message with the additional scientific operations available in this class.
+	 *
+	 * parseOperation(const std::string&):
+	 * 	- Overrides the base function to interpret extra commands like 'sin', 'log', etc.
+	 * 	- It determines which member function to call based on user input.
+	 */ 
 	virtual void welcome() const override;
 	virtual void parseOperation(const std::string& input) override;
-
+	
+	/* These are additional mathematical functions not avaible in the basic calculator:
+	 * These functions would likely: 
+	 * 	- Prompt the suser for input,
+	 * 	- Use std::sin, std::log, etc. from <cmath>
+	 * 	- Store the results in the inherited result variable.
+	 */
 	void sin();
 	void cos();
 	void tan();
@@ -22,4 +53,6 @@ public:
 	void pow();
 };
 
+
+// Ends the considtional compiliation block that started with #ifndef SCIENTIFIC_H
 #endif 
